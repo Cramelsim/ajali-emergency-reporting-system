f66e14f76c1784a27ec9c67599343ba26327fb50
Melody Simwa <simwamelody@gmail.com>
2026-03-18 17:42:40 +0300

    add imports in init.py

---
+++ ajali/backend/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from flask_mail import Mail
+from dotenv import load_dotenv
+import os
+import redis
+from celery import Celery
