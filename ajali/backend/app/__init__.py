540f32853673521f325d419103284fe4036d34c6
Melody Simwa <simwamelody@gmail.com>
2026-03-19 22:37:46 +0300

    file upload configuration

--- ajali/backend/app/__init__.py
+++ ajali/backend/app/__init__.py
@@ -32,3 +32,7 @@ def create_app(config_class=None):
     app.config['MAIL_USE_TLS'] = True
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+
+    # File upload configuration
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB max file size
