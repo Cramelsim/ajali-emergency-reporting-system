e0d0c13ab731a791c95ec3afa7dbf4cd18e42925
Melody Simwa <simwamelody@gmail.com>
2026-03-22 18:30:19 +0300

    add imports in auth.py

---
+++ ajali/backend/app/routes/auth.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db
+from app.models import User
+from datetime import timedelta
+import re
