937662c8cea53bfb56ef424379d5e156ba3ba13a
Melody Simwa <simwamelody@gmail.com>
2026-03-29 22:14:14 +0300

    register blueprint im admin.py route

--- ajali/backend/app/routes/admin.py
+++ ajali/backend/app/routes/admin.py
@@ -5,3 +5,5 @@ from app.models import Incident, User, Notification
 from app.utils.decorators import admin_required
 from app.utils.helpers import send_status_update_email, send_status_update_sms
 from datetime import datetime
+
+admin_bp = Blueprint('admin', __name__)
