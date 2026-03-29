78495a17aef98029a3ed78a50d761dff1279b421
Melody Simwa <simwamelody@gmail.com>
2026-03-30 01:04:37 +0300

    add if statement to Send SMS if user has phone number

--- ajali/backend/app/routes/admin.py
+++ ajali/backend/app/routes/admin.py
@@ -132,3 +132,7 @@ def send_status_notifications(incident_id, old_status, new_status):
          # Send email if user has email
         if user.email:
             send_status_update_email(user.email, incident, old_status, new_status)
+
+        # Send SMS if user has phone number
+        if user.phone_number:
+            send_status_update_sms(user.phone_number, incident, old_status, new_status)
