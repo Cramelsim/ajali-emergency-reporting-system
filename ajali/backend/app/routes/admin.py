299d52e5ee6dda6818a030c816315befc06a14b8
Melody Simwa <simwamelody@gmail.com>
2026-03-29 22:13:21 +0300

    add imports to admin.py route

---
+++ ajali/backend/app/routes/admin.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app import db, celery
+from app.models import Incident, User, Notification
+from app.utils.decorators import admin_required
+from app.utils.helpers import send_status_update_email, send_status_update_sms
+from datetime import datetime
