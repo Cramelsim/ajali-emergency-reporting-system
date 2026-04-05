76ca42b3762213f429f4935d39a1caecc90f64ff
Melody Simwa <simwamelody@gmail.com>
2026-04-05 23:25:30 +0300

    add imports in index.js

---
+++ ajali/frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { store } from './redux/store';
+import App from './App';
+import './index.css';
+
