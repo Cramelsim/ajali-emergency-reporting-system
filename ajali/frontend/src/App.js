cd55873fd5af1fb814d0a2871f57533098d23854
Melody Simwa <simwamelody@gmail.com>
2026-04-10 21:30:02 +0300

    import pages in app.js

--- ajali/frontend/src/App.js
+++ ajali/frontend/src/App.js
@@ -3,3 +3,15 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+
+i
+// Pages
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import IncidentList from './pages/IncidentList';
+import IncidentDetail from './pages/IncidentDetail';
+import CreateIncident from './pages/CreateIncident';
+import EditIncident from './pages/EditIncident';
+import AdminDashboard from './pages/AdminDashboard';
+
