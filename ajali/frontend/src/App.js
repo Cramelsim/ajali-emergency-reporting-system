a287004e68ceb0e2346fc0edd8987e168913ae3c
Melody Simwa <simwamelody@gmail.com>
2026-04-10 21:29:22 +0300

    add imports in app.js

---
+++ ajali/frontend/src/App.js
@@ -0,0 +1,5 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
