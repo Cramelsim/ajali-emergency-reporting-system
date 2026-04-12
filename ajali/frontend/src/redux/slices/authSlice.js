1af41a09f91ee6a5a65313bf8f0a7064b962bd91
Melody Simwa <simwamelody@gmail.com>
2026-04-12 20:42:50 +0300

    define login in authSlice.js

--- ajali/frontend/src/redux/slices/authSlice.js
+++ ajali/frontend/src/redux/slices/authSlice.js
@@ -3,3 +3,19 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`${API_URL}/auth/login`, {
+        email,
+        password,
+      });
+      localStorage.setItem('access_token', response.data.access_token);
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data?.error || 'Login failed');
+    }
+  }
+);
