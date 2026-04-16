b6de444fa5d9ec68defb2f064b58df995f49aa4c
Melody Simwa <simwamelody@gmail.com>
2026-04-16 23:50:25 +0300

    register API in incident slice

--- ajali/frontend/src/redux/slices/incidentSlice.js
+++ ajali/frontend/src/redux/slices/incidentSlice.js
@@ -1,3 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
